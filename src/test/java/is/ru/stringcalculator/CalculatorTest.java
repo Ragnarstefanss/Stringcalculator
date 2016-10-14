5badc992cee1b6acaef86b04f90d627f0d426114
Ragnarstefanss <ragnars15@ru.is>
2016-10-14 18:06:41 +0000

    added a new number to testMultipleNumbers and it still works

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -26,7 +26,7 @@ public class CalculatorTest {
 
 	@Test
     public void testMultipleNumbers(){
-    	assertEquals(6, Calculator.add("1,2,3"));
+    	assertEquals(11, Calculator.add("1,2,3,5"));
     }
 
 }
